8c3bb1cef4f7fa8fe823311aaddb6cce4f4fb513
mahsaShv <Msheikhi@ce.sharif.edu>
2022-04-30 15:54:21 +0430

    Temp function fixed

--- src/JavaCup.java
+++ src/JavaCup.java
@@ -23,12 +23,15 @@ public class JavaCup {
         else { System.out.println("NO"); }
     }
     public static void temp() {
-        ArrayList a = new ArrayList();
+
+        int counter = 0;
+        int[] a = new int[200000000];
+
         for (int i = 0; i < 10000; i++)
-        {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[counter] = i + j;
+                counter += 1;
             }
-        }
+
     }
 }
